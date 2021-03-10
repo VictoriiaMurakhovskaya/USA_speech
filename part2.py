3596a03863605bb54d0cae6fe7693d02b27820a7
Denys Murakhovskyi <dmurakhowsky@gmail.com>
2021-03-10 23:32:28 +0200

    Command line processing

--- part2.py
+++ part2.py
@@ -11,9 +11,13 @@ from itertools import chain
 import matplotlib.pyplot as plt
 
 import sys
+from _collections import OrderedDict
 
+import re
 
-speech_dir = "us_presidential_speeches/"
+# значение директорий по умолчанию
+default_in_path = "us_presidential_speeches"
+default_out_path = 'output_std'
 
 
 def president_terms(corpus, vocab=None):
@@ -31,35 +35,76 @@ def president_terms(corpus, vocab=None):
 
 
 def get_data():
-    if sys.argv[1] != '--terms':
-        print('Incorrect arguments')
+    """
+    Получение параметров из командной строки и запись этих параметров в словарь
+    :return: словарь с параметрами
+    """
+    pos = {}
+    values = {}
+    args = ['--terms', '--path', '--title', '--output']
+
+    # получение позиций названий аргументов типа --title
+    for item in args:
+        if item in sys.argv:
+            pos.update({item[2:]: sys.argv.index(item)})
+    pos = OrderedDict({k: v for k, v in sorted(pos.items(), key=lambda item: item[1])})
+
+    # получение значений аргументов с использованием полученных позиций
+    for i in range(len(list(pos.keys())) - 1):
+        values.update({list(pos.keys())[i]: sys.argv[pos[list(pos.keys())[i]] + 1: pos[list(pos.keys())[i + 1]]]})
+    values.update({list(pos.keys())[i + 1]: sys.argv[pos[list(pos.keys())[i + 1]] + 1:]})
+
+    # проверка значений, подстановка умолчаний
+    if 'terms' not in values.keys():
+        print('No terms defined')
         sys.exit(1)
 
-    try:
-        i_title = sys.argv.index('--title')
-        terms = sys.argv[2:i_title]
-        title = sys.argv[i_title + 1]
-    except:
-        print('No title is given')
-        terms = sys.argv[2:]
-        title = ""
+    if 'path' not in values.keys():
+        values.update({'path': default_in_path})
+    else:
+        values['path'] = values['path'][0]
 
-    return terms, title
+    if 'output' not in values.keys():
+        filename = '_'.join(values['terms']) + '.png'
+        filename = filename.replace(' ', '_')
+        values.update({'output': default_out_path + '/' + filename})
+    else:
+        values['output'] = values['output'][0]
+
+    if 'title' not in values.keys():
+        values.update({'title': None})
+    else:
+        values['title'] = values['title'][0]
+
+    return values
 
 
 def main():
-    terms, title = get_data()
-    get_score(terms, title)
+    """
+    Основной метод программы
+    Вызывается, если скрипт запущен как основной модуль
+    :return:
+    """
+    get_score(get_data())
 
 
-def get_score(terms, title):
+def get_score(values):
+    """
+    Метод получений корпуса текстов и JSON
+    :param values: слловарь значений параметров
+    :return:
+    """
     # чтение набора JSON файлов в словарь
     # дата - ключ, текст речи - значение
+    speech_dir = values['path']
     txts = {}
-    for file in os.listdir(speech_dir):
-        with open(speech_dir + file, "r") as infile:
-            speech = json.load(infile)
-            txts.update({speech["Date"]: speech["Speech"]})
+    try:
+        for file in os.listdir(speech_dir):
+            with open(speech_dir + '/' + file, "r") as infile:
+                speech = json.load(infile)
+                txts.update({speech["Date"]: speech["Speech"]})
+    except IOError:
+        sys.exit(1)
 
     # создание датафрейма из словаря
     df = pd.DataFrame({'Date': list(txts.keys()), 'Speech': list(txts.values())})
@@ -79,12 +124,12 @@ def get_score(terms, title):
 
     # применяем написанную функцию получения tf-ids score к каждой строке датафрейма
     # для применения apply параметр vocab переведен в kwargs
-    df_years['Score'] = df_years.Speech.apply(president_terms, vocab=terms)
+    df_years['Score'] = df_years.Speech.apply(president_terms, vocab=values['terms'])
 
     # разделение кортежей score на отдельные колонки
     # создаем датафрейм, в котором scores разделены по двум колонкам
     score_frame = pd.DataFrame(np.array(df_years.Score.values.tolist()),
-                               columns=terms,
+                               columns=values['terms'],
                                index=df_years.index)
 
     # объединяем полученный датафрейм score с исходным фреймом
@@ -98,10 +143,24 @@ def get_score(terms, title):
 
     # генерируем график встроенными средствами pandas
     # график отображается на канве matplotlib по умолчанию
-    df_years.plot.line(x='Year', y=terms)
+    df_years.plot.line(x='Year', y=values['terms'])
+
+    # добавление заголовка, если он установлен
+    if values['title']:
+        plt.title(values['title'])
+
+    # определение названий директории и файла из параметра output
+    # запись графического изображения в файл с использование matplotlib
+    try:
+        path = re.search(r'[^\/]+$', values['output'])
+        path = values['output'].replace(path.group(0), '')
+        if not os.path.exists(path):
+            os.mkdir(path)
+        plt.savefig(values['output'])
+    except IOError:
+        print('Image save error')
 
     # отображаем фигуру matplotlib
-    plt.title(title)
     plt.show()
 
 
